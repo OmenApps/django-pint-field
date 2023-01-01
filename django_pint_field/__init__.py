0a848f9dde74b0f9a945b8e06dcfd23260aa62ed
Jack Linke <jacklinke@gmail.com>
2023-01-01 13:54:35 -0500

    Add comparitor column to each field

--- django_pint_field/__init__.py
+++ django_pint_field/__init__.py
@@ -11,6 +11,11 @@ __version__ = "0.0.1"
 # e.g.: x, y = IntegerPintDBField(magnitude=1, unit="xyz")
 
 
+def base_unit_magnitude(value):
+    new_value = value.ito_base_units()
+    return new_value.magnitude
+
+
 def get_IntegerPintDBField():
     return register_composite("integer_pint_field", connection.cursor().cursor, globally=True).type
 
@@ -25,29 +30,32 @@ def get_DecimalPintDBField():
 
 def integer_pint_field_adapter(value):
     return AsIs(
-        "(%s::integer, %s::text)::integer_pint_field"
+        "(%s::decimal, %s::integer, %s::text)::integer_pint_field"
         % (
-            adapt(value.magnitude).getquoted(),
-            adapt(str(value.units)).getquoted(),
+            adapt(base_unit_magnitude(value)),
+            adapt(value.magnitude),
+            adapt(str(value.units)),
         )
     )
 
 
 def big_integer_pint_field_adapter(value):
     return AsIs(
-        "(%s::bigint, %s::text)::big_integer_pint_field"
+        "(%s::decimal, %s::bigint, %s::text)::big_integer_pint_field"
         % (
-            adapt(value.magnitude).getquoted(),
-            adapt(str(value.units)).getquoted(),
+            adapt(base_unit_magnitude(value)),
+            adapt(value.magnitude),
+            adapt(str(value.units)),
         )
     )
 
 
 def decimal_pint_field_adapter(value):
     return AsIs(
-        "(%s::decimal, %s::text)::decimal_pint_field"
+        "(%s::decimal, %s::decimal, %s::text)::decimal_pint_field"
         % (
-            adapt(value.magnitude).getquoted(),
-            adapt(str(value.units)).getquoted(),
+            adapt(base_unit_magnitude(value)),
+            adapt(value.magnitude),
+            adapt(str(value.units)),
         )
     )
--- django_pint_field/migrations/0001_create_composite_fields.py
+++ django_pint_field/migrations/0001_create_composite_fields.py
@@ -1,4 +1,12 @@
 from django.db import migrations
+from django.conf import settings
+
+
+comparator_precision = getattr(settings, "DJANGO_PINT_FIELD_COMPARATOR_PRECISION", 28)
+comparator_scale = getattr(settings, "DJANGO_PINT_FIELD_COMPARATOR_SCALE", 16)
+
+decimal_pint_field_precision = getattr(settings, "DJANGO_PINT_FIELD_DECIMAL_FIELD_PRECISION", 16)
+decimal_pint_field_scale = getattr(settings, "DJANGO_PINT_FIELD_DECIMAL_FIELD_SCALE", 4)
 
 
 class Migration(migrations.Migration):
@@ -8,9 +16,15 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             sql=[
-                ("CREATE TYPE integer_pint_field as (magnitude integer, units text)"),
-                ("CREATE TYPE big_integer_pint_field as (magnitude bigint, units text)"),
-                ("CREATE TYPE decimal_pint_field as (magnitude decimal, units text)"),
+                (
+                    f"CREATE TYPE integer_pint_field as (comparator decimal({comparator_precision}, {comparator_scale}), magnitude integer, units text)"
+                ),
+                (
+                    f"CREATE TYPE big_integer_pint_field as (comparator decimal({comparator_precision}, {comparator_scale}), magnitude bigint, units text)"
+                ),
+                (
+                    f"CREATE TYPE decimal_pint_field as (comparator decimal({comparator_precision}, {comparator_scale}), magnitude decimal({decimal_pint_field_precision}, {decimal_pint_field_scale}), units text)"
+                ),
             ],
             reverse_sql=[
                 ("DROP TYPE integer_pint_field CASCADE;"),
