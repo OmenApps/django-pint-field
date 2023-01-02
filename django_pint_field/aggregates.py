c67c560d5cedd2f70a2462f8daacc70676c56ffe
Jack Linke <jacklinke@gmail.com>
2023-01-02 00:56:26 -0500

    Bump version

--- django_pint_field/aggregates.py
+++ django_pint_field/aggregates.py
@@ -1,7 +1,5 @@
 from django.db.models import Aggregate
-from django.db.models.functions.mixins import (
-    NumericOutputFieldMixin,
-)
+from django.db.models.functions.mixins import NumericOutputFieldMixin
 from django.db.models.fields import IntegerField
 from django.db.models.expressions import Star
 
--- django_pint_field/helper.py
+++ django_pint_field/helper.py
@@ -1,20 +1,20 @@
-from pint import DimensionalityError, UnitRegistry
+from pint import DimensionalityError
 from typing import List
 
 
-def check_matching_unit_dimension(ureg: UnitRegistry, default_unit: str, units_to_check: List[str]) -> None:
+def check_matching_unit_dimension(registry, default_unit: str, units_to_check: List[str]) -> None:
     """
     Check if all units_to_check have the same Dimension like the default_unit
     If not
     :raise DimensionalityError
     """
 
-    default_unit = getattr(ureg, default_unit)
+    default_unit = getattr(registry, default_unit)
 
     print("check_matching_unit_dimension")
 
     for unit_string in units_to_check:
-        unit = getattr(ureg, unit_string)
+        unit = getattr(registry, unit_string)
         print(
             f"check_matching_unit_dimension unit: {unit} {unit.dimensionality}, "
             f"default_unit: {default_unit} {default_unit.dimensionality}, "
