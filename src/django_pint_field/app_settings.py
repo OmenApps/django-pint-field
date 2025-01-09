19cfc2341495605f0e94ff19bdaf50720cbb3188
jacklinke <jacklinke@gmail.com>
2025-01-09 18:02:52 -0500

    Add ability to set default format

--- src/django_pint_field/app_settings.py
+++ src/django_pint_field/app_settings.py
@@ -6,6 +6,7 @@ from pint import set_application_registry
 
 
 # Define default unit register
+# See: https://pint.readthedocs.io/en/stable/advanced/defining.html
 DJANGO_PINT_FIELD_UNIT_REGISTER = getattr(settings, "DJANGO_PINT_FIELD_UNIT_REGISTER", UnitRegistry())
 # Set as default application registry for i.e. for pickle
 set_application_registry(DJANGO_PINT_FIELD_UNIT_REGISTER)
@@ -13,6 +14,11 @@ set_application_registry(DJANGO_PINT_FIELD_UNIT_REGISTER)
 # Define decimal precision
 # If `DJANGO_PINT_FIELD_DECIMAL_PRECISION` is set to an int value greater than 0, the project's
 #   decimalprecision will be set to that value. Otherwise, it is left as default (28 digits).
-#
 # See: https://docs.python.org/3/library/decimal.html#mitigating-round-off-error-with-increased-precision
 DJANGO_PINT_FIELD_DECIMAL_PRECISION = getattr(settings, "DJANGO_PINT_FIELD_DECIMAL_PRECISION", 0)
+
+# Define default format
+# If `DJANGO_PINT_FIELD_DEFAULT_FORMAT` is set to a string value, the project's default format
+#   will be set to that value. Otherwise, it is left as default ("D").
+# See: https://pint.readthedocs.io/en/stable/user/formatting.html for other options.
+DJANGO_PINT_FIELD_DEFAULT_FORMAT = getattr(settings, "DJANGO_PINT_FIELD_DEFAULT_FORMAT", "D")
--- src/django_pint_field/units.py
+++ src/django_pint_field/units.py
@@ -1,7 +1,9 @@
-"""Define the unit register that is used in the package."""
+"""Define the unit register and default format used in the package."""
 
+from .app_settings import DJANGO_PINT_FIELD_DEFAULT_FORMAT
 from .app_settings import DJANGO_PINT_FIELD_UNIT_REGISTER
 
 
 # The unit register that was defined in the settings (shortcurt)
 ureg = DJANGO_PINT_FIELD_UNIT_REGISTER
+ureg.formatter.default_format = DJANGO_PINT_FIELD_DEFAULT_FORMAT
